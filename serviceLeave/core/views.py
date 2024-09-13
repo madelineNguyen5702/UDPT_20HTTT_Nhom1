b73d90f96eec3883126c1d025a740ed14a03f236
Oxaloala <nguyenngoc2001.22@gmail.com>
2024-09-13 22:26:36 +0700

    handleRequestLeaveManager, handleRequestUpdateTimesheet, handleRequestWFHManager

--- serviceLeave/core/views.py
+++ serviceLeave/core/views.py
@@ -6,6 +6,9 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from django.contrib.sessions.models import Session
 from .serializers import *
+from datetime import date
+from datetime import datetime
+
 
 class TimesheetView(viewsets.ViewSet):
     @action(detail=False, methods=['post'], url_path='checkIn')
@@ -177,7 +180,92 @@ class TimesheetView(viewsets.ViewSet):
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
         
+    #------------------------------------------------------------------------
 
+    @action(detail=False, methods=['post'], url_path='handleRequestUpdateTimesheet')
+    def handleRequestUpdateTimesheet(self, request):
+        manv = request.data.get('MaNV')
+        ngay = request.data.get('Ngay')
+        checkin = request.data.get('Checkin')
+        updatecheckin = request.data.get('updateCheckin')  # Thay đổi chỗ này
+        checkout = request.data.get('Checkout')
+        updatecheckout = request.data.get('updateCheckout')  # Thay đổi chỗ này
+        trangthaimoi = request.data.get('TrangThai')
+        idnguoiduyet = request.data.get('IDNguoiDuyet')
+
+        if not manv or not ngay or not idnguoiduyet:
+            return Response({'error': 'MaNV, Ngay, và IDNguoiDuyet là bắt buộc'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Format Ngay to only contain date part
+        try:
+            ngay = datetime.strptime(ngay.split('T')[0], '%Y-%m-%d').date()
+        except ValueError:
+            return Response({'error': 'Ngay không hợp lệ'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Set NgayDuyet to the current date
+        ngayduyet = date.today().strftime('%Y-%m-%d')
+
+        try:
+            queryset = RequestUpdate.objects.filter(
+                MaNV=manv,
+                Ngay=ngay,
+                IDNguoiDuyet=idnguoiduyet,
+                TrangThai="Chờ"
+            )
+
+            if not queryset.exists():
+                return Response({'error': 'Không có bảng ghi phù hợp'}, status=status.HTTP_404_NOT_FOUND)
+
+            # Kiểm tra xem giá trị updateCheckin/updateCheckout có hợp lệ không
+            if updatecheckin:
+                try:
+                    datetime.strptime(updatecheckin, '%H:%M')  # Kiểm tra định dạng HH:MM
+                except ValueError:
+                    return Response({'error': 'updateCheckin có định dạng không hợp lệ. Phải là HH:MM'}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                updatecheckin = checkin  # Nếu không có giá trị mới, dùng giá trị cũ
+
+            if updatecheckout:
+                try:
+                    datetime.strptime(updatecheckout, '%H:%M')  # Kiểm tra định dạng HH:MM
+                except ValueError:
+                    return Response({'error': 'updateCheckout có định dạng không hợp lệ. Phải là HH:MM'}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                updatecheckout = checkout  # Nếu không có giá trị mới, dùng giá trị cũ
+
+            # Cập nhật bản ghi RequestUpdate
+            num_updated = queryset.update(
+                Checkin=updatecheckin,
+                Checkout=updatecheckout,
+                TrangThai=trangthaimoi,
+                NgayDuyet=ngayduyet,
+            )
+
+            if num_updated == 0:
+                return Response({'message': 'Không có bản ghi nào được cập nhật'}, status=status.HTTP_404_NOT_FOUND)
+
+            try:
+                # Cập nhật bản ghi Timesheet
+                querysett = Timesheet.objects.filter(MaNV=manv, Ngay=ngay)
+                num_updated = querysett.update(
+                    Checkin=updatecheckin,
+                    Checkout=updatecheckout
+                )
+
+                if num_updated == 0:
+                    return Response({'message': 'Không có bản ghi nào được cập nhật trong Timesheet'}, status=status.HTTP_404_NOT_FOUND)
+
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            serializer = RequestUpdateSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
 
 class LeaveView(viewsets.ViewSet):
     #------------------------------------------------------------------------
@@ -236,58 +324,118 @@ class LeaveView(viewsets.ViewSet):
         except RequestLeave.DoesNotExist:
             return Response({'error': 'No employees found'}, status=status.HTTP_404_NOT_FOUND)
     #------------------------------------------------------------------------
+    # @action(detail=False, methods=['post'], url_path='handleRequestLeaveManager')
+    # def handleRequestLeaveManager(self, request):
+    #     manv = request.data.get('MaNV')
+    #     ngaygui = request.data.get('NgayGui')
+    #     ngayrequest = request.data.get('NgayRequest')
+    #     ngayduyet = request.data.get('NgayDuyet')
+    #     trangthaimoi = request.data.get('TrangThai')
+    #     idnguoiduyet = request.data.get('IDNguoiDuyet')
+
+    #     if not manv:
+    #         return Response({'error': 'MaNV is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    #     try:
+    #         # Lọc bản ghi cần cập nhật
+    #         queryset = RequestLeave.objects.filter(
+    #             MaNV=manv,
+    #             NgayGui=ngaygui,
+    #             NgayRequest=ngayrequest,
+    #             TrangThai="Chờ",
+    #             IDNguoiDuyet=idnguoiduyet
+    #         )
+
+    #         if not queryset.exists():
+    #             return Response({'error': 'Không có yêu cầu phù hợp'}, status=status.HTTP_404_NOT_FOUND)
+
+    #         # Cập nhật bản ghi
+    #         num_updated = queryset.update(
+    #             NgayDuyet=ngayduyet,
+    #             TrangThai=trangthaimoi
+    #         )
+
+    #         if num_updated == 0:
+    #             return Response({'message': 'No records updated or found'}, status=status.HTTP_404_NOT_FOUND)
+
+    #         # Tải lại queryset sau khi cập nhật
+    #         updated_queryset = RequestLeave.objects.filter(
+    #             MaNV=manv,
+    #             NgayGui=ngaygui,
+    #             NgayRequest=ngayrequest,
+    #             TrangThai=trangthaimoi,
+    #             IDNguoiDuyet=idnguoiduyet
+    #         )
+
+    #         # Serialize lại dữ liệu sau khi cập nhật
+    #         serializer = RequestLeaveSerializer(updated_queryset, many=True)
+
+    #         # Trả về dữ liệu sau khi cập nhật
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    #     except Exception as e:
+    #         return Response({'error': str(e)}, status=status.HTTP)
+
+
     @action(detail=False, methods=['post'], url_path='handleRequestLeaveManager')
+
     def handleRequestLeaveManager(self, request):
         manv = request.data.get('MaNV')
-        ngaygui = request.data.get('NgayGui')
-        ngayrequest = request.data.get('NgayRequest')
-        ngayduyet = request.data.get('NgayDuyet')
+        ngayrequest = request.data.get('NgayRequest')  # Added to filter the correct record
+        idnguoiduyet = request.data.get('IDNguoiDuyet')  # Ensure this is the correct field name
         trangthaimoi = request.data.get('TrangThai')
-        idnguoiduyet = request.data.get('IDNguoiDuyet')
 
-        if not manv:
-            return Response({'error': 'MaNV is required'}, status=status.HTTP_400_BAD_REQUEST)
+        if not manv or not ngayrequest or not idnguoiduyet:
+            return Response({'error': 'MaNV, NgayRequest, và IDNguoiDuyet là bắt buộc'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # # Set NgayDuyet to the current date
+        # ngayduyet = date.today().strftime('%Y-%m-%d')  # Format the date as YYYY-MM-DD
+
+        # Format NgayRequest to only contain date part
+        try:
+            ngayrequest = datetime.strptime(ngayrequest.split('T')[0], '%Y-%m-%d').date()
+        except ValueError:
+            return Response({'error': 'NgayRequest không hợp lệ'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Set NgayDuyet to the current date
+        ngayduyet = date.today().strftime('%Y-%m-%d')
+
 
         try:
-            # Lọc bản ghi cần cập nhật
+            # Filter using the correct field names
             queryset = RequestLeave.objects.filter(
                 MaNV=manv,
-                NgayGui=ngaygui,
                 NgayRequest=ngayrequest,
-                TrangThai="Chờ",
-                IDNguoiDuyet=idnguoiduyet
+                IDNguoiDuyet=idnguoiduyet,
+                TrangThai="Chờ"
             )
 
             if not queryset.exists():
                 return Response({'error': 'Không có yêu cầu phù hợp'}, status=status.HTTP_404_NOT_FOUND)
 
-            # Cập nhật bản ghi
+            # Update the record
             num_updated = queryset.update(
                 NgayDuyet=ngayduyet,
                 TrangThai=trangthaimoi
             )
 
             if num_updated == 0:
-                return Response({'message': 'No records updated or found'}, status=status.HTTP_404_NOT_FOUND)
+                return Response({'message': 'Không có bản ghi nào được cập nhật'}, status=status.HTTP_404_NOT_FOUND)
 
-            # Tải lại queryset sau khi cập nhật
+            # Retrieve the updated records
             updated_queryset = RequestLeave.objects.filter(
                 MaNV=manv,
-                NgayGui=ngaygui,
                 NgayRequest=ngayrequest,
-                TrangThai=trangthaimoi,
                 IDNguoiDuyet=idnguoiduyet
             )
 
-            # Serialize lại dữ liệu sau khi cập nhật
             serializer = RequestLeaveSerializer(updated_queryset, many=True)
-
-            # Trả về dữ liệu sau khi cập nhật
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP)
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+#--------------------------------------------------------------------------------------------------------------------------------------------
 
 class WFHView(viewsets.ViewSet):
     #------------------------------------------------------------------------
@@ -347,52 +495,108 @@ class WFHView(viewsets.ViewSet):
         except RequestWFH.DoesNotExist:
             return Response({'error': 'No employees found'}, status=status.HTTP_404_NOT_FOUND)
     #------------------------------------------------------------------------
+    # @action(detail=False, methods=['post'], url_path='handleRequestWFHManager')
+    # def handleRequestWFHManager(self, request):
+    #     manv = request.data.get('MaNV')
+    #     ngaygui = request.data.get('NgayGui')
+    #     ngayrequest = request.data.get('NgayRequest')
+    #     ngayduyet = request.data.get('NgayDuyet')
+    #     trangthaimoi = request.data.get('TrangThai')
+
+    #     if not manv:
+    #         return Response({'error': 'MaNV is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    #     try:
+    #         # Lọc bản ghi cần cập nhật
+    #         queryset = RequestWFH.objects.filter(
+    #             MaNV=manv,
+    #             NgayGui=ngaygui,
+    #             NgayRequest=ngayrequest,
+    #             TrangThai="Chờ"
+    #         )
+
+    #         if not queryset.exists():
+    #             return Response({'error': 'Không có yêu cầu phù hợp'}, status=status.HTTP_404_NOT_FOUND)
+
+    #         # Cập nhật bản ghi
+    #         num_updated = queryset.update(
+    #             NgayDuyet=ngayduyet,
+    #             TrangThai=trangthaimoi
+    #         )
+
+    #         if num_updated == 0:
+    #             return Response({'message': 'No records updated or found'}, status=status.HTTP_404_NOT_FOUND)
+
+    #         # Tải lại queryset sau khi cập nhật
+    #         updated_queryset = RequestWFH.objects.filter(
+    #             MaNV=manv,
+    #             NgayGui=ngaygui,
+    #             NgayRequest=ngayrequest,
+    #             TrangThai=trangthaimoi
+    #         )
+
+    #         # Serialize lại dữ liệu sau khi cập nhật
+    #         serializer = RequestWFHSerializer(updated_queryset, many=True)
+
+    #         # Trả về dữ liệu sau khi cập nhật
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    #     except Exception as e:
+    #         return Response({'error': str(e)}, status=status.HTTP)
+
     @action(detail=False, methods=['post'], url_path='handleRequestWFHManager')
     def handleRequestWFHManager(self, request):
         manv = request.data.get('MaNV')
-        ngaygui = request.data.get('NgayGui')
-        ngayrequest = request.data.get('NgayRequest')
-        ngayduyet = request.data.get('NgayDuyet')
+        ngayrequest = request.data.get('NgayRequest')  # Added to filter the correct record
+        idnguoiduyet = request.data.get('IDNguoiDuyet')  # Ensure this is the correct field name
         trangthaimoi = request.data.get('TrangThai')
-        idnguoiduyet = request.data.get('IDNguoiDuyet')
 
-        if not manv:
-            return Response({'error': 'MaNV is required'}, status=status.HTTP_400_BAD_REQUEST)
+        if not manv or not ngayrequest or not idnguoiduyet:
+            return Response({'error': 'MaNV, NgayRequest, và IDNguoiDuyet là bắt buộc'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # # Set NgayDuyet to the current date
+        # ngayduyet = date.today().strftime('%Y-%m-%d')  # Format the date as YYYY-MM-DD
+
+        # Format NgayRequest to only contain date part
+        try:
+            ngayrequest = datetime.strptime(ngayrequest.split('T')[0], '%Y-%m-%d').date()
+        except ValueError:
+            return Response({'error': 'NgayRequest không hợp lệ'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Set NgayDuyet to the current date
+        ngayduyet = date.today().strftime('%Y-%m-%d')
+
 
         try:
-            # Lọc bản ghi cần cập nhật
+            # Filter using the correct field names
             queryset = RequestWFH.objects.filter(
                 MaNV=manv,
-                NgayGui=ngaygui,
                 NgayRequest=ngayrequest,
+                IDNguoiDuyet=idnguoiduyet,
                 TrangThai="Chờ"
             )
 
             if not queryset.exists():
                 return Response({'error': 'Không có yêu cầu phù hợp'}, status=status.HTTP_404_NOT_FOUND)
 
-            # Cập nhật bản ghi
+            # Update the record
             num_updated = queryset.update(
                 NgayDuyet=ngayduyet,
                 TrangThai=trangthaimoi
             )
 
             if num_updated == 0:
-                return Response({'message': 'No records updated or found'}, status=status.HTTP_404_NOT_FOUND)
+                return Response({'message': 'Không có bản ghi nào được cập nhật'}, status=status.HTTP_404_NOT_FOUND)
 
-            # Tải lại queryset sau khi cập nhật
+            # Retrieve the updated records
             updated_queryset = RequestWFH.objects.filter(
                 MaNV=manv,
-                NgayGui=ngaygui,
                 NgayRequest=ngayrequest,
-                TrangThai=trangthaimoi
+                IDNguoiDuyet=idnguoiduyet
             )
 
-            # Serialize lại dữ liệu sau khi cập nhật
             serializer = RequestWFHSerializer(updated_queryset, many=True)
-
-            # Trả về dữ liệu sau khi cập nhật
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP)
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
